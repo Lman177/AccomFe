dcc266195df69d628670829bd33d02e8fd97248b
Lman177 <namnh.bi12-308@st.usth.edu.vn>
2024-04-05 22:11:24 +0700

    1st commit: add room to db

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import "/node_modules/bootstrap/dist/js/bootstrap.min.js"
+import AddRoom from './assets/components/room/AddRoom'
+import './App.css'
+
+function App() {
+  return (
+  <>
+  <AddRoom/>
+  </>
+  )
+}
+
+export default App
