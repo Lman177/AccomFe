11f569662441117563604774b41836790c151e04
Lman177 <namnh.bi12-308@st.usth.edu.vn>
2024-07-11 11:03:10 +0700

    fix server

--- src/assets/components/utils/ApiFunctions.js
+++ src/assets/components/utils/ApiFunctions.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 export const api = axios.create({
     // baseURL : "https://deploybe-657a31bc3d6c.herokuapp.com"
-    baseURL : "http://http://35.238.250.153/:8080"
+    baseURL : "http://35.238.250.153:8080"
 })
 
 export const getHeader = () => {
