0052a4fa2b91a1690bc9dd5104842ada55582453
Lman177 <namnh.bi12-308@st.usth.edu.vn>
2024-07-12 09:42:16 +0700

    change port sv

--- src/assets/components/admin/RevenueChart .jsx
+++ src/assets/components/admin/RevenueChart .jsx
@@ -17,7 +17,7 @@ const RevenueChart = () => {
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await axios.get('http://35.238.250.153:8080/bookings/profit');
+                const response = await axios.get('http://152.67.115.24:8080/bookings/profit');
                 processChartData(response.data);
             } catch (error) {
                 console.error('Error fetching data:', error);
