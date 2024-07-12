d4cfb890878e1e7169a74a7af2021c16f1b85aa1
Lman177 <namnh.bi12-308@st.usth.edu.vn>
2024-07-12 09:39:03 +0700

    change sv port

--- src/assets/components/utils/ApiFunctions.js
+++ src/assets/components/utils/ApiFunctions.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 export const api = axios.create({
     // baseURL : "https://deploybe-657a31bc3d6c.herokuapp.com"
-    baseURL : "http://35.238.250.153:8080"
+    baseURL : "http://152.67.115.24:8080"
 })
 
 export const getHeader = () => {
